f4623fd1c577cc7707818c94a7c5382f01d79895
zahidparviz <pervaizzahid55@gmail.com>
2024-02-16 17:28:52 +0500

    updated arrayproject to decrease its time & space complexity

--- Arrays/TemperatureProject.java
+++ Arrays/TemperatureProject.java
@@ -6,6 +6,7 @@ public class TemperatureProject {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         TemperatureProject tp = new TemperatureProject();
+        int avg = 0;
 
         System.out.print("Enter the number of days for temperature: ");
         int days = sc.nextInt();
@@ -17,21 +18,16 @@ public class TemperatureProject {
         for(int i=0; i<temperature.length; i++) {
             System.out.print("Enter Day " + (i+1) + "s temperature: " );
             temperature[i] = sc.nextInt();
+            avg += temperature[i];
         }
         System.out.println("\nValues inserted successfully!\n");
 
-        tp.findAverageAndDaysAboveAverage(temperature);
+        tp.findDaysAboveAverage(temperature, avg);
 
         sc.close();
     }
 
-    public void findAverageAndDaysAboveAverage(int[] temperature) {
-        int avg = 0;
-
-        for(int num: temperature) {
-            avg += num;
-        }
-
+    public void findDaysAboveAverage(int[] temperature, int avg) {
         avg = avg/temperature.length;
 
         System.out.println("Average is: " + avg);
